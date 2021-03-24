3059abf246fbe5e21cf5060c91f43a47d9a779d9
crelbee <crlllbee@gmail.com>
2021-03-24 15:34:46 +0300

    add CounterServer

---
+++ jersey/src/main/java/CounterServiceConfig.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+public class CounterServiceConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> classes = new HashSet<>();
+        classes.add(CounterServlet.class);
+        return classes;
+    }
+
+}
--- jersey/src/main/java/JerseyApplication.java
+++ jersey/src/main/java/JerseyApplication.java
@@ -1,6 +1,9 @@
 public class JerseyApplication {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws Exception {
     // run, Jetty, run!
+    CounterServer server = new CounterServer();
+    server.start();
+    server.join();
   }
 }
